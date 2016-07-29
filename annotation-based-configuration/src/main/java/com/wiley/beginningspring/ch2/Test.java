07122a04494a620d93ea7885d70868ff46740775
jianleizhang <jianleizhang@Ctrip.com>
2016-07-29 10:01:41 +0800

    init

---
+++ annotation-based-configuration/src/main/java/com/wiley/beginningspring/ch2/Test.java
@@ -0,0 +1,10 @@
+package com.wiley.beginningspring.ch2;
+
+/**
+ * Created by jianleizhang on 2016/7/29.
+ */
+public class Test {
+    public static void main(String args[]){
+            System.out.println("hello world!");
+        }
+}
